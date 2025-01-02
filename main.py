540bb1c54997b438ff64a73dbb44217c611d5c8c
Sakin Kirti <sakin.arya.kirti@gmail.com>
2025-01-01 19:35:13 -0800

    added bayes rule for inference, but i think i did something wrong

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-from model import Graph
+from model import ProbabilisticGraph
 import matplotlib.pyplot as plt
 
-g = Graph()
+pgm = ProbabilisticGraph()
 
 # a testing dataset from kaggle
 df = pd.read_csv("/Users/sakinkirti/Downloads/heart_disease.csv")
@@ -11,17 +11,34 @@ for col in ["currentSmoker", "BPMeds", "prevalentHyp", "diabetes"]:
     df[col] = df[col].map(lambda x: np.where(x == 1, "yes", "no"))
 
 # load just the nodes from 
-g.load_nodes_from_dataframe(df)
+pgm.load_nodes_from_dataframe(df)
 
-# add specific edges
-g.add_edge("cigsPerDay", "currentSmoker")
-g.add_edge("cigsPerDay", "heartRate")
-g.add_edge("BMI", "heartRate")
-g.add_edge("glucose", "diabetes")
-g.add_edge("totChol", "prevalentStroke")
-g.add_edge("BMI", "diabetes")
+# add specific edges (used a doctor's help to make these connections), want to implement some automated edge creation - need to do some research on how to do this
+pgm.add_edge("cigsPerDay", "currentSmoker")
+pgm.add_edge("cigsPerDay", "heartRate")
+pgm.add_edge("BMI", "heartRate")
+pgm.add_edge("glucose", "diabetes")
+pgm.add_edge("totChol", "prevalentStroke")
+pgm.add_edge("BMI", "diabetes")
+pgm.add_edge("cigsPerDay", "Heart_ stroke")
+pgm.add_edge("diabetes", "Heart_ stroke")
+pgm.add_edge("prevalentHyp", "Heart_ stroke")
+pgm.add_edge("education", "prevalentHyp")
+pgm.add_edge("education", "glucose")
+pgm.add_edge("education", "totChol")
+pgm.add_edge("education", "cigsPerDay")
+pgm.add_edge("education", "BMI")
+pgm.add_edge("Gender", "Heart_ stroke")
+pgm.add_edge("age", "diaBP")
+pgm.add_edge("age", "sysBP")
+pgm.add_edge("prevalentHyp", "diaBP")
+pgm.add_edge("prevalentHyp", "sysBP")
+pgm.add_edge("BPMeds", "diaBP")
+pgm.add_edge("BPMeds", "sysBP")
+pgm.add_edge("BPMeds", "heartRate")
+pgm.add_edge("prevalentHyp", "BPMeds")
 
-vis = g.visualize_graph(42)
+vis = pgm.visualize_graph(1234)
 plt.show()
 
-print(g)
+print(pgm.conditional_query("Heart_ stroke", "yes", conditional_nodes=["Gender", "diabetes", "prevalentHyp"], conditional_values=["Female", "yes", "yes"]))
--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 from typing import Literal
 
-class Graph:
+class ProbabilisticGraph:
     
     def __init__(self):
         
@@ -89,6 +90,14 @@ class Graph:
         return ax
 
     def load_nodes_from_dataframe(self, dataframe:pd.DataFrame):
+        """loads nodes from a given dataframe
+
+        Args:
+            dataframe (pd.DataFrame): the dataframe to load nodes from
+
+        Returns:
+            None: stores the data in graph format
+        """
         
         # basic inference of the type of data for easy automated loading
         def infer_storage_type(values:pd.Series):
@@ -105,6 +114,44 @@ class Graph:
                 self.add_node(col, storage_type=st, values=dataframe[col].unique().tolist(), probs=dataframe[col].value_counts().values/len(dataframe))
             else:
                 self.add_node(col, storage_type=st, mean=dataframe[col].mean(), standard_deviation=dataframe[col].std())
+
+    def infer_edges(self):
+        pass
+
+    def query(self, node:str, value, operation:str="equals"):
+        """with base value, calculate the probability of a particular node
+
+        Args:
+            node (str): the node to calculate probability for
+            value (str, int, float): the value to check probability for
+            operation (str, optional): if value is numerical, the operation to check probability for. Defaults to "equals".
+
+        Returns:
+            float: the probability of the particular value at the given node
+        """
+        
+        # if the node is categorical, just return the probability of that value
+        if type(self.graph[node]) is self.CategoricalNode and type(value) is str:
+            return self.graph[node].get_probs(value)
+        # if it's continuous, return the probability of the operation to the value
+        elif type(self.graph[node]) is self.ContinuousNode and type(value) is not str:
+            return self.graph[node].get_probs(value, operation)
+
+    def conditional_query(self, node:str, value, operation:str="equals", conditional_nodes:list=None, conditional_values:list=None):
+        
+        # if there are no conditions, this reverts to regular inference
+        if conditional_nodes is None:
+            print("You provided no conditional nodes, defaulting to normal inference")
+            return self.inference(node, value, operation)
+        
+        # Bayes' Rule with multiple conditions
+        #if type(node) is self.CategoricalNode:
+        num = np.prod(np.array([self.graph[node].get_probs(value) * self.graph[cn].get_probs(cv) for cn,cv in zip(conditional_nodes, conditional_values)] + [self.graph[node].get_probs(value)**(len(conditional_nodes)+1)]))
+        den = np.sum(np.array([
+            np.prod(np.array([self.graph[node].get_probs(value) * self.graph[cn].get_probs(cv) for cn,cv in zip(conditional_nodes, conditional_values)] + [self.graph[node].get_probs(value)**(len(conditional_nodes)+1)])),
+            np.prod(np.array([(1 - self.graph[node].get_probs(value)) * self.graph[cn].get_probs(cv) for cn,cv in zip(conditional_nodes, conditional_values)] + [(1 - self.graph[node].get_probs(value))**(len(conditional_nodes)+1)]))
+        ]))
+        return num / den
 
     class CategoricalNode:
         
@@ -140,6 +187,8 @@ class Graph:
             
         def get_name(self): return self.name
         
+        def get_probs(self, value:str): return self.table[value]
+        
     class ContinuousNode:
         
         def __init__(self, name:str, mean:float, standard_deviation:float):
@@ -174,4 +223,25 @@ class Graph:
         def get_outgoing(self): return self.outgoing
             
         def get_name(self): return self.name
+        
+        def get_probs(self, value:float, operation:Literal["equals", "greater", "less"]):
+            """computes probability of a particular continuous value
+
+            Args:
+                value (float): the value to check probability for
+                operation (Literal["equals", "greater", "less"]): the operation to check the probability for
+
+            Returns:
+                float: the probability of the operation for the input value
+            """
+            
+            if operation == "equals":
+                z = (value - self.mean) / self.sd
+                return stats.norm.cdf(z+0.005) - stats.norm.cdf(z-0.005)
+            elif operation == "greater":
+                z = (value - self.mean) / self.sd
+                return 1 - stats.norm.cdf(z)
+            elif operation == "less":
+                z = (value - self.mean) / self.sd
+                return stats.norm.cdf(z)
     
